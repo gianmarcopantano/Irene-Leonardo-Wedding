1da07059e7336dc059f653b1932cdb70b981e86d
Gianmarco Pantano <>
2025-10-01 10:08:40 +0200

    versione 4.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ae880899b218dbc7a79144fb24589161",
+const RESOURCES = {"flutter_bootstrap.js": "a2e36f28560cb9a6db4dbd11939d0d90",
 "version.json": "3d0a8b3e454aa8006e8b32fefcd8e5e3",
 "index.html": "470c4ce9a4eeb1ef31f24a6f4fffe1a1",
 "/": "470c4ce9a4eeb1ef31f24a6f4fffe1a1",
-"main.dart.js": "4775bc730f7e5dc4b8aac2c055f622b1",
+"main.dart.js": "c6a653e2a7dbab3044d0f24a6e96b412",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "icons/wedding.png": "d905a05e8425ade8843b24eccbecc827",
 "manifest.json": "93038eb13262e2de97208550a37ec216",
